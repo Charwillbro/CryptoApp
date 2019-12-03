abaa43da1997657b7aaf78c98cbc81f52065dfc5
Charwillbro <35745538+Charwillbro@users.noreply.github.com>
2019-12-03 16:35:40 -0600

    Adjusted display data annotations in currency model

--- CryptoApp/Models/CurrencyModel.cs
+++ CryptoApp/Models/CurrencyModel.cs
@@ -20,19 +20,22 @@ namespace CryptoApp.Models
         //Price of cryptocurrency in USD
         [JsonProperty]
         [Display(Name = "Current Price: ")]
-        [DisplayFormat(DataFormatString = "{0:$#.#######}")]
+        [DataType(DataType.Currency)]
+       // [DisplayFormat(DataFormatString = "{0:$#.#######}")]
         public decimal PRICE { get; set; }
 
         //Highest Price of cryptocurrency today
         [JsonProperty]
         [Display(Name = "Highest Price Today: ")]
-        [DisplayFormat(DataFormatString = "{0:$#.#######}")]
+        [DataType(DataType.Currency)]
+        // [DisplayFormat(DataFormatString = "{0:$#.#######}")]
         public decimal HIGHDAY { get; set; }
 
         //Lowest Price of cryptocurrency today
         [JsonProperty]
         [Display(Name = "Lowest Price Today: ")]
-        [DisplayFormat(DataFormatString = "{0:$#.#######}")]
+        [DataType(DataType.Currency)]
+        //[DisplayFormat(DataFormatString = "{0:$#.#######}")]
         public decimal LOWDAY { get; set; }
 
         //Amount the price has changed today in dollars
